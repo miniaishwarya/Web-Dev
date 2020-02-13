0ef5fa9a73618d0e727b0c8535d76873d2588b99
miniaishwarya <miniash11@gmail.com>
2020-02-13 23:21:07 +0530

    counter in react

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-function App(){
-  return <h1> hello! </h1>
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    const currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount + 1,
+    });
+  };
+
+  decrement = () => {
+    const currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount - 1,
+    });
+  };
+
+  render() {
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+    </div>
+  }
 }
 
 render(<App />, document.getElementById('root'));
